9997369661ec4027541beca3b35a7bbd535b6ff4
Corey Hemminger <hemminger@hotmail.com>
2024-03-12 18:06:01 -0500

    test fixes
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- kitchen-tests/test/integration/end-to-end/_chef_client_config.rb
+++ kitchen-tests/test/integration/end-to-end/_chef_client_config.rb
@@ -4,10 +4,12 @@ client_rb = if os.windows?
               "/etc/chef/client.rb"
             end
 
-describe file(client_rb) do
-  it { should exist }
-  its("content") { should match(%r{chef_server_url "https://localhost/organizations/test"}) }
-  its("content") { should match(/chef_license "accept"/) }
-  its("content") { should match(%r{rubygems_url "https://rubygems.org/"}) }
-  its("content") { should match(/require 'aws-sdk'/) }
-end unless os.darwin? # TODO: Figure out why this test fails on macos
+unless os.darwin? # TODO: Figure out why this test fails on macOS
+  describe file(client_rb) do
+    it { should exist }
+    its("content") { should match(%r{chef_server_url "https://localhost/organizations/test"}) }
+    its("content") { should match(/chef_license "accept"/) }
+    its("content") { should match(%r{rubygems_url "https://rubygems.org/"}) }
+    its("content") { should match(/require 'aws-sdk'/) }
+  end
+end
