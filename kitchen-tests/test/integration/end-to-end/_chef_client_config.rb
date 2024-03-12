3fe23a341f81979e1d384eeaea188ab03e2d55ea
Corey Hemminger <hemminger@hotmail.com>
2024-03-11 20:51:03 -0500

    test fixes
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- kitchen-tests/test/integration/end-to-end/_chef_client_config.rb
+++ kitchen-tests/test/integration/end-to-end/_chef_client_config.rb
@@ -5,8 +5,8 @@ client_rb = if os.windows?
             end
 
 describe file(client_rb) do
-  its("content") { should match(%r{chef_server_url "https://localhost"}) }
+  its("content") { should match(/chef_server_url "https:\/\/localhost"/) }
   its("content") { should match(/chef_license "accept"/) }
-  its("content") { should match(%r{rubygems_url "https://rubygems.org/"}) }
+  its("content") { should match(/rubygems_url "https:\/\/rubygems.org\/"/) }
   its("content") { should match(/require 'aws-sdk'/) }
 end
